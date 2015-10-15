8b12e4bece0c9c5599c0bf2495ab368fda514370
Looly <loolly@gmail.com>
2015-10-15 16:28:14 +0800

    Add PathView

---
+++ src/main/java/com/xiaoleilu/hulu/render/view/PageView.java
@@ -0,0 +1,16 @@
+package com.xiaoleilu.hulu.render.view;
+
+/**
+ * 重定向到页面
+ * @author Looly
+ *
+ */
+public class PageView extends PathView{
+	
+	public PageView() {
+	}
+
+	public PageView(String view) {
+		super(view);
+	}
+}
---
+++ src/main/java/com/xiaoleilu/hulu/render/view/PathView.java
@@ -0,0 +1,16 @@
+package com.xiaoleilu.hulu.render.view;
+
+/**
+ * 转到指定路径的View
+ * @author Looly
+ *
+ */
+public class PathView extends ForwardView{
+	
+	public PathView() {
+	}
+
+	public PathView(String path) {
+		super(path);
+	}
+}

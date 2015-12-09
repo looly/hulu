3cb71b4425be256c98f0c91f349c806ac0ec5c23
Looly <loolly@gmail.com>
2015-12-09 15:03:21 +0800

    Add HtmlFileView

---
+++ src/main/java/com/xiaoleilu/hulu/render/view/HtmlFileView.java
@@ -0,0 +1,24 @@
+package com.xiaoleilu.hulu.render.view;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.xiaoleilu.hulu.HuluSetting;
+import com.xiaoleilu.hulu.exception.ActionRuntimeException;
+import com.xiaoleilu.hutool.FileUtil;
+
+/**
+ * HTML文件，读取文件内容作为HTML返回
+ * @author Looly
+ *
+ */
+public class HtmlFileView extends HtmlView{
+	public HtmlFileView(File file) {
+		super();
+		try {
+			this.text = FileUtil.readString(file, HuluSetting.charset);
+		} catch (IOException e) {
+			throw new ActionRuntimeException(e);
+		}
+	}
+}

6851fbc86a4bd00364c4f79c6611983b2ea3db32
Looly <loolly@gmail.com>
2016-11-19 14:37:11 +0000

    del tomcat embed

--- src/test/java/com/xiaoleilu/huludemo/Server.java
+++ src/test/java/com/xiaoleilu/huludemo/Server.java
@@ -2,7 +2,6 @@ package com.xiaoleilu.huludemo;
 
 import com.xiaoleilu.hulu.exception.ServerException;
 import com.xiaoleilu.hulu.server.EmbedJettyServer;
-import com.xiaoleilu.hulu.server.EmbedTomcatServer;
 
 /**
  * 内嵌的Jetty服务器
@@ -11,14 +10,13 @@ import com.xiaoleilu.hulu.server.EmbedTomcatServer;
  */
 public class Server {
 	public static void main(String[] args) throws ServerException {
-		String serverName = "tomcat";
+		String serverName = "jetty";
 		
 		switch (serverName) {
 			case "jetty":
 				EmbedJettyServer.startup(true);
 				break;
 			case "tomcat":
-				EmbedTomcatServer.startup(true);
 				break;
 			default:
 				break;
